5d9280198c6c8dbb0980e7af76b625995463b6be
Aishat Yusuff <aishat.yusuff@ibm.com>
2023-09-08 18:15:46 +0100

    adds modal form functionalities

--- script.js
+++ script.js
@@ -137,3 +137,25 @@ document.addEventListener("keydown", function (e) {
   if (e.key === "ArrowRight") nextSlide();
   if (e.key === "ArrowLeft") prevSlide();
 });
+
+//Modal form
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+btnOpenModal.addEventListener("click", openModal);
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
