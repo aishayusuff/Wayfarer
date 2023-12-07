fe870b44054a2e6119472a29a01bb0a079bfcbb7
aishayusuff <aishatyusuffe@gmail.com>
2023-12-07 18:48:26 +0000

    finishes hamburger styling

--- modules/hamburgerMenu.js
+++ modules/hamburgerMenu.js
@@ -1,20 +1,22 @@
 export const opensHamburger = function (containerSelector) {
   const menuLinks = document.querySelector(".hamburger-nav-links");
   const hamburgerIcon = document.querySelector(".hamburger-icon");
+  const hamburgerOverlay = document.querySelector(".hamburger-overlay");
 
   containerSelector.addEventListener("click", function () {
     menuLinks.classList.toggle("open");
     hamburgerIcon.classList.toggle("open");
+    hamburgerOverlay.classList.toggle("blur");
   });
 
-  if(menuLinks.classList.contains(".open")){
+  if (menuLinks.classList.contains(".open")) {
     smoothScrollToTop();
   }
 
-  const smoothScrollToTop = function(){
+  const smoothScrollToTop = function () {
     window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    })
-  }
+      top: 0,
+      behavior: "smooth",
+    });
+  };
 };
